52e8b53efb3cc1a265218af57b6f56ff2516498c
Tutku Ince <tutku.ince@outlook.com>
2025-02-12 01:02:11 +0300

    challenge

--- sql/basics/string-functions.sql
+++ sql/basics/string-functions.sql
@@ -47,5 +47,13 @@ FROM employees;
 SELECT
     email,
     split_part(email, '@', 2)
-FROM employees
+FROM employees;
 
+-- Your company has been purchased by UDEMY and all e-mail addresses need to be converted into a new format.
+-- The new format will be in the form of "surname.ad@udemy.com" (eg smith.john@udemy.com).
+-- The IT department asked you to prepare a list of available and new e-mail addresses.
+-- Gaps will be removed for employees with double names or surnames (eg dehaan.lex@udemy.com for 'Lex de Haan').
+SELECT CONCAT(first_name, ' ', last_name)                                                                 as full_name,
+       email                                                                                              as old_email,
+       CONCAT(LOWER(REPLACE(last_name, ' ', '')), '.', LOWER(REPLACE(first_name, ' ', '')), '@udemy.com') as new_email
+FROM employees;
