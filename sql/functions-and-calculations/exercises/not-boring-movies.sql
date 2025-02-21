cf306b509b2e40032c8a3603371ed24ad4260155
Tutku Ince <tutku.ince@outlook.com>
2025-02-21 22:32:41 +0300

    Not Boring Movies

---
+++ sql/functions-and-calculations/exercises/not-boring-movies.sql
@@ -0,0 +1,24 @@
+-- +----------------+----------+
+-- | Column Name    | Type     |
+-- +----------------+----------+
+-- | id             | int      |
+-- | movie          | varchar  |
+-- | description    | varchar  |
+-- | rating         | float    |
+-- +----------------+----------+
+-- id is the primary key (column with unique values) for this table.
+-- Each row contains information about the name of a movie, its genre, and its rating.
+-- rating is a 2 decimal places float in the range [0, 10]
+--
+-- Write a solution to report the movies with an odd-numbered ID and a description that is not "boring".
+--
+-- Return the result table ordered by rating in descending order.
+
+SELECT
+    id,
+    movie,
+    description,
+    rating
+FROM Cinema
+WHERE MOD(id, 2) != 0 AND description != 'boring'
+ORDER BY rating DESC
