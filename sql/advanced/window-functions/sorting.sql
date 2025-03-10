a98c742d9c63b46f45e7bb88d95d05c3ab7fa5b3
Tutku Ince <tutku.ince@outlook.com>
2025-03-10 22:10:17 +0300

    information about ROW_NUMBER

---
+++ sql/advanced/window-functions/sorting.sql
@@ -0,0 +1,11 @@
+-- ROW_NUMBER
+-- Determines the ordinal number of the current row within a group of rows, counting from 1, based on the ORDER BY expression in the OVER clause.
+-- If the optional PARTITION BY clause is present, the ordinal numbers are reset for each group of rows.
+-- Rows with equal values for the ORDER BY expressions receive the different row numbers nondeterministically.
+-- ROW_NUMBER () OVER
+-- (
+--     [ PARTITION BY expr_list ]
+--     [ ORDER BY order_list ]
+-- )
+
+-- List the orders of each customer in date order.
