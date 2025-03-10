6cb61b17728581434b931152e20d710b336253da
Tutku Ince <tutku.ince@outlook.com>
2025-03-10 22:24:06 +0300

    DENSE_RANK

--- sql/advanced/window-functions/sorting.sql
+++ sql/advanced/window-functions/sorting.sql
@@ -36,3 +36,19 @@ SELECT
     ROW_NUMBER() OVER (PARTITION BY contactid ORDER BY to_char(bookingdate, 'YYYY-MM-DD')) as row_num,
     RANK() OVER (PARTITION BY contactid ORDER BY to_char(bookingdate, 'YYYY-MM-DD')) as ran_num
 FROM bookings;
+
+-- DENSE_RANK window function
+-- The DENSE_RANK window function determines the rank of a value in a group of values, based on the ORDER BY expression in the OVER clause.
+-- If the optional PARTITION BY clause is present, the rankings are reset for each group of rows.
+-- Rows with equal values for the ranking criteria receive the same rank.
+
+-- List the orders of each customer in date order.
+
+SELECT
+    contactid,
+    bookingdate,
+    to_char(bookingdate, 'YYYY-MM-DD') as booking_day,
+    ROW_NUMBER() OVER (PARTITION BY contactid ORDER BY to_char(bookingdate, 'YYYY-MM-DD')) as row_num,
+    RANK() OVER (PARTITION BY contactid ORDER BY to_char(bookingdate, 'YYYY-MM-DD')) as rank_num,
+    DENSE_RANK() OVER (PARTITION BY contactid ORDER BY to_char(bookingdate, 'YYYY-MM-DD')) as dense_rank_num
+FROM bookings;
