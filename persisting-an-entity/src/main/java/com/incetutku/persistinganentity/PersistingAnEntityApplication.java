b8b310f4b38a72bf10e01add2398857d8c03cfb1
Tutku Ince <tutku.ince@outlook.com>
2025-03-11 22:03:26 +0300

    project initializer

---
+++ persisting-an-entity/src/main/java/com/incetutku/persistinganentity/PersistingAnEntityApplication.java
@@ -0,0 +1,13 @@
+package com.incetutku.persistinganentity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersistingAnEntityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PersistingAnEntityApplication.class, args);
+    }
+
+}
---
+++ persisting-an-entity/src/test/java/com/incetutku/persistinganentity/PersistingAnEntityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.incetutku.persistinganentity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersistingAnEntityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
