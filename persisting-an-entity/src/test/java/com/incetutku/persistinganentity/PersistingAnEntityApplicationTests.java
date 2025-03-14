83109e7ea80f3981f4d95f774a4f34bd5ad9a634
Tutku Ince <tutku.ince@outlook.com>
2025-03-14 19:21:26 +0300

    CRUD test

--- persisting-an-entity/src/test/java/com/incetutku/persistinganentity/PersistingAnEntityApplicationTests.java
+++ persisting-an-entity/src/test/java/com/incetutku/persistinganentity/PersistingAnEntityApplicationTests.java
@@ -1,13 +1,39 @@
 package com.incetutku.persistinganentity;
 
+import com.incetutku.persistinganentity.entity.Student;
+import com.incetutku.persistinganentity.repository.StudentRepository;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class PersistingAnEntityApplicationTests {
 
+    @Autowired
+    private StudentRepository studentRepository;
+
     @Test
-    void contextLoads() {
+    void testCrud() {
+        // Create
+        Student student = new Student("Tutku Ince", "2025AN20123");
+        Student returnedStudent = studentRepository.save(student);
+        assertNotNull(returnedStudent.getId());
+
+        // Update
+        returnedStudent.setName("Utku Ince");
+        Student updatedStudent = studentRepository.save(returnedStudent);
+        assertEquals("Utku Ince", updatedStudent.getName());
+
+        // Read
+        Student foundStudent = studentRepository.findById(1L).orElseThrow(RuntimeException::new);
+        assertEquals(1L, foundStudent.getId());
+
+        // Delete
+        studentRepository.delete(foundStudent);
+        assertFalse(studentRepository.existsById(1L));
     }
 
 }
