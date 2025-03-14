33352f2483985370b56ddb2206b89a901f7857b9
Tutku Ince <tutku.ince@outlook.com>
2025-03-14 18:50:58 +0300

    info about states

--- persisting-an-entity/src/main/java/com/incetutku/persistinganentity/client/StudentClient.java
+++ persisting-an-entity/src/main/java/com/incetutku/persistinganentity/client/StudentClient.java
@@ -18,13 +18,18 @@ public class StudentClient implements ApplicationRunner {
     @Override
     public void run(ApplicationArguments args) throws Exception {
         // Persisting a student
-        Student student = new Student("Tutku Ince", "2025AN50123");
-        Student savedStudent = studentRepository.save(student);    // update=> merge() | save=> persist()
+        Student student = new Student("Tutku Ince", "2025AN50123"); // transient state
+        Student savedStudent = studentRepository.save(student);    // save=> persist() persistent state
+        // after transaction commit complete, the entity manager will be closed.
+        // once that happens, our student object and savedStudent will move from the persistent state to the detached state.
+        // it's no longer being managed by our entity manager
+        // Once the line 21 finishes its execution, this savedStudent variable and also this student variable they both would be
+        // referencing to the same student objects in the detached state.
 
         // transient state -> persistent state(save) -> detached state (after save)
 
         // Updating a student
         savedStudent.setName("Utku Ince");
-        Student updatedStudent = studentRepository.save(savedStudent);
+        Student updatedStudent = studentRepository.save(savedStudent);  // update=> merge()
     }
 }
