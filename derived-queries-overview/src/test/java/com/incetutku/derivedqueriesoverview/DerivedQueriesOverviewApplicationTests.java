1d1a35213de60ee2acdb02b66b8c6ae6c8056846
Tutku Ince <tutku.ince@outlook.com>
2025-03-15 17:00:15 +0300

    test method

--- derived-queries-overview/src/test/java/com/incetutku/derivedqueriesoverview/DerivedQueriesOverviewApplicationTests.java
+++ derived-queries-overview/src/test/java/com/incetutku/derivedqueriesoverview/DerivedQueriesOverviewApplicationTests.java
@@ -1,13 +1,47 @@
 package com.incetutku.derivedqueriesoverview;
 
-import org.junit.jupiter.api.Test;
+import com.incetutku.derivedqueriesoverview.entity.Student;
+import com.incetutku.derivedqueriesoverview.repository.StudentRepository;
+import org.junit.jupiter.api.*;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @SpringBootTest
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class DerivedQueriesOverviewApplicationTests {
 
+    @Autowired
+    private StudentRepository studentRepository;
+
+    @BeforeAll
+    void populateDB() {
+        Student s1 = new Student("Alissa Simmons", "2022AN50123");
+        Student s2 = new Student("Tora Bialek", "2007TE50698");
+        Student s3 = new Student("Sharon Ambriz", "2025SI50213");
+        Student s4 = new Student("Tianna Armentrout", "2022TU50471");
+        Student s5 = new Student("Angelica Zapien", "2022AE50802");
+        Student s6 = new Student("Aarti Evatt", "2022AT50385");
+
+        studentRepository.save(s1);
+        studentRepository.save(s2);
+        studentRepository.save(s3);
+        studentRepository.save(s4);
+        studentRepository.save(s5);
+        studentRepository.save(s6);
+    }
+
+    @AfterAll
+    void dePopulateDB() {
+        studentRepository.deleteAll();
+    }
+
     @Test
-    void contextLoads() {
+    void testFindByEnrollmentId() {
+        Student student = studentRepository.findByEnrollmentId("2025SI50213");
+        assertEquals("2025SI50213", student.getEnrollmentId());
+        System.out.println(student);
     }
 
 }
