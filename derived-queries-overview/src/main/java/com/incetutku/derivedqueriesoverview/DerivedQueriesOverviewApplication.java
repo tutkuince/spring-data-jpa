56c621bd4c3a23e85f6029bc8136842859fa397d
Tutku Ince <tutku.ince@outlook.com>
2025-03-15 16:43:20 +0300

    project initializer

---
+++ derived-queries-overview/src/main/java/com/incetutku/derivedqueriesoverview/DerivedQueriesOverviewApplication.java
@@ -0,0 +1,13 @@
+package com.incetutku.derivedqueriesoverview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DerivedQueriesOverviewApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DerivedQueriesOverviewApplication.class, args);
+    }
+
+}
---
+++ derived-queries-overview/src/test/java/com/incetutku/derivedqueriesoverview/DerivedQueriesOverviewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.incetutku.derivedqueriesoverview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DerivedQueriesOverviewApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
