8ee2e270aff81b254e83ae2af925bd4b26247722
Tutku Ince <tutku.ince@outlook.com>
2025-03-19 20:46:10 +0300

    information about Query by Example

--- query-by-example/src/main/java/com/incetutku/querybyexample/repository/UserRepository.java
+++ query-by-example/src/main/java/com/incetutku/querybyexample/repository/UserRepository.java
@@ -4,4 +4,7 @@ import com.incetutku.querybyexample.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+    // Query By Example (QBE) Limitations:
+    // Only supports starts/contains/ends/regex matching for strings and exact matching for other property types
+    // No support for nested/grouped property constraints, such as "firstname = ?0 or (firstname  = ?1 and lastname = ?2)"
 }
