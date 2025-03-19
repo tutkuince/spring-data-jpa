10862a45ac4dedf328a0eed54ceb1dbb97db03da
Tutku Ince <tutku.ince@outlook.com>
2025-03-19 14:54:51 +0300

    project initializer

---
+++ query-by-example/src/main/java/com/incetutku/querybyexample/QueryByExampleApplication.java
@@ -0,0 +1,13 @@
+package com.incetutku.querybyexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QueryByExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QueryByExampleApplication.class, args);
+    }
+
+}
---
+++ query-by-example/src/test/java/com/incetutku/querybyexample/QueryByExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.incetutku.querybyexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QueryByExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
