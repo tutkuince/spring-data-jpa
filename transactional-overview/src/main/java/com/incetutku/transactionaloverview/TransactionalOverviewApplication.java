e418dee12340aab82bc459a83f32165ddb2b5c68
Tutku Ince <tutku.ince@outlook.com>
2025-03-20 15:38:59 +0300

    project initializer and config

---
+++ transactional-overview/src/main/java/com/incetutku/transactionaloverview/TransactionalOverviewApplication.java
@@ -0,0 +1,13 @@
+package com.incetutku.transactionaloverview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactionalOverviewApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TransactionalOverviewApplication.class, args);
+    }
+
+}
---
+++ transactional-overview/src/test/java/com/incetutku/transactionaloverview/TransactionalOverviewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.incetutku.transactionaloverview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactionalOverviewApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
