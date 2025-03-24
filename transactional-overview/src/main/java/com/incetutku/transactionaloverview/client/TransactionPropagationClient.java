3e878e8b5c665ee7e288bd4169741dc236298b8b
Tutku Ince <tutku.ince@outlook.com>
2025-03-24 21:43:13 +0300

    Client

---
+++ transactional-overview/src/main/java/com/incetutku/transactionaloverview/client/TransactionPropagationClient.java
@@ -0,0 +1,21 @@
+package com.incetutku.transactionaloverview.client;
+
+import com.incetutku.transactionaloverview.service.ItemService;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class TransactionPropagationClient implements ApplicationRunner {
+
+    private final ItemService itemService;
+
+    public TransactionPropagationClient(ItemService itemService) {
+        this.itemService = itemService;
+    }
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        itemService.persistAnItem();
+    }
+}
