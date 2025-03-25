297cb94045545707b2b3abd4c98065a040ffd6b1
Tutku Ince <tutku.ince@outlook.com>
2025-03-25 17:01:21 +0300

    User1Client and User2Clinet threads have been added

---
+++ transactional-overview/src/main/java/com/incetutku/transactionaloverview/client/User1Client.java
@@ -0,0 +1,20 @@
+package com.incetutku.transactionaloverview.client;
+
+import com.incetutku.transactionaloverview.entity.Guide;
+import com.incetutku.transactionaloverview.service.CollegeManagementService;
+
+public class User1Client implements Runnable {
+
+    private final CollegeManagementService collegeManagementService;
+
+    public User1Client(CollegeManagementService collegeManagementService) {
+        this.collegeManagementService = collegeManagementService;
+    }
+
+    @Override
+    public void run() {
+        Guide guide = collegeManagementService.findGuideById(2L);
+        guide.setSalary(3000);
+        collegeManagementService.updateGuide(guide);
+    }
+}
---
+++ transactional-overview/src/main/java/com/incetutku/transactionaloverview/client/User2Client.java
@@ -0,0 +1,20 @@
+package com.incetutku.transactionaloverview.client;
+
+import com.incetutku.transactionaloverview.entity.Guide;
+import com.incetutku.transactionaloverview.service.CollegeManagementService;
+
+public class User2Client implements Runnable {
+
+    private final CollegeManagementService collegeManagementService;
+
+    public User2Client(CollegeManagementService collegeManagementService) {
+        this.collegeManagementService = collegeManagementService;
+    }
+
+    @Override
+    public void run() {
+        Guide guide = collegeManagementService.findGuideById(2L);
+        guide.setSalary(4000);
+        collegeManagementService.updateGuide(guide);
+    }
+}
