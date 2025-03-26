26670a462ba19f4b6320c309e7371cad5f456963
Tutku Ince <tutku.ince@outlook.com>
2025-03-26 14:10:03 +0300

    new methods have been added

--- transactional-overview/src/main/java/com/incetutku/transactionaloverview/repository/GuideRepository.java
+++ transactional-overview/src/main/java/com/incetutku/transactionaloverview/repository/GuideRepository.java
@@ -1,7 +1,19 @@
 package com.incetutku.transactionaloverview.repository;
 
 import com.incetutku.transactionaloverview.entity.Guide;
+import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface GuideRepository extends JpaRepository<Guide, Long> {
+
+    @Lock(LockModeType.PESSIMISTIC_READ)
+    @Query("SELECT g.name, g.salary FROM Guide as g")
+    List<Object[]> getNameAndSalaryOfAll();
+
+    @Query("SELECT SUM(g.salary) FROM Guide as g")
+    Long calculateSumOfAllSalaries();
 }
