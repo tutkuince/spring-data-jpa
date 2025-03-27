24ac536b08493e1b4fa4a862c443b71c0d31557b
Tutku Ince <tutku.ince@outlook.com>
2025-03-27 23:30:45 +0300

    use modifiying

--- modifiying-queries/src/main/java/com/incetutku/modifiyingqueries/repository/UserRepository.java
+++ modifiying-queries/src/main/java/com/incetutku/modifiyingqueries/repository/UserRepository.java
@@ -2,6 +2,17 @@ package com.incetutku.modifiyingqueries.repository;
 
 import com.incetutku.modifiyingqueries.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+
+//    @Transactional    // derived queries delete the data one by one. it is not a bulk operations.
+//    Integer deleteByLevel(Integer level); // bulk operations run a bit faster.
+
+    @Transactional
+    @Modifying
+    @Query("DELETE FROM User as u WHERE u.level =: level")
+    Integer deleteInBulByLevel(Integer level);
 }
