8e9d4b682054feff1081eff49f324e1a66aaade4
Tutku Ince <tutku.ince@outlook.com>
2025-03-27 23:58:06 +0300

    Cleint

---
+++ modifiying-queries/src/main/java/com/incetutku/modifiyingqueries/client/ModifyingClient.java
@@ -0,0 +1,28 @@
+package com.incetutku.modifiyingqueries.client;
+
+import com.incetutku.modifiyingqueries.service.UserService;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ModifyingClient implements ApplicationRunner {
+
+    private final UserService userService;
+
+    public ModifyingClient(UserService userService) {
+        this.userService = userService;
+    }
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        // populate db
+        userService.persistUsers();
+
+        // deleting users one-by-one by level
+        userService.deleteUsersByLevel();
+
+        // deleting users in-bulk by level
+        userService.deleteUsersInBulkByLevel();
+    }
+}
