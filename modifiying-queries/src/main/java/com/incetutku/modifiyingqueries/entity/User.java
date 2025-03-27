2c61a8b691fe0e7f7d55a65028ea2b83169f28a1
Tutku Ince <tutku.ince@outlook.com>
2025-03-27 23:13:37 +0300

    user entity

--- modifiying-queries/src/main/java/com/incetutku/modifiyingqueries/entity/User.java
+++ modifiying-queries/src/main/java/com/incetutku/modifiyingqueries/entity/User.java
@@ -1,2 +1,95 @@
-package com.incetutku.modifiyingqueries.entity;public class User {
+package com.incetutku.modifiyingqueries.entity;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+    @Column(name = "registration_date")
+    private LocalDate registrationDate;
+
+    private String email;
+    private Integer level;
+
+    @Column(name = "active")
+    private Boolean isActive;
+
+    public User() {
+    }
+
+    public User(String username, LocalDate registrationDate, String email, Integer level, Boolean isActive) {
+        this.username = username;
+        this.registrationDate = registrationDate;
+        this.email = email;
+        this.level = level;
+        this.isActive = isActive;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public LocalDate getRegistrationDate() {
+        return registrationDate;
+    }
+
+    public void setRegistrationDate(LocalDate registrationDate) {
+        this.registrationDate = registrationDate;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Integer getLevel() {
+        return level;
+    }
+
+    public void setLevel(Integer level) {
+        this.level = level;
+    }
+
+    public Boolean getActive() {
+        return isActive;
+    }
+
+    public void setActive(Boolean active) {
+        isActive = active;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", registrationDate=" + registrationDate +
+                ", email='" + email + '\'' +
+                ", level=" + level +
+                ", isActive=" + isActive +
+                '}';
+    }
 }
