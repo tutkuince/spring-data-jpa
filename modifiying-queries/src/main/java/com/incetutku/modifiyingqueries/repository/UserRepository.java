65d0cd44f345f3a9d898beac4a061984a5937c09
Tutku Ince <tutku.ince@outlook.com>
2025-03-27 23:48:40 +0300

    more info

--- modifiying-queries/src/main/java/com/incetutku/modifiyingqueries/repository/UserRepository.java
+++ modifiying-queries/src/main/java/com/incetutku/modifiyingqueries/repository/UserRepository.java
@@ -15,4 +15,6 @@ public interface UserRepository extends JpaRepository<User, Long> {
     @Modifying
     @Query("DELETE FROM User as u WHERE u.level =: level")
     Integer deleteInBulkByLevel(Integer level);
+    // Please make sure that the modifying query that you want to run to perform your bulk operation is being run within a transaction.
+    // otherwise it will throw an exception.
 }
