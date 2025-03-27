a336f48cabb3ae3c9ea28996d002cf9e170a6a40
Tutku Ince <tutku.ince@outlook.com>
2025-03-27 23:09:49 +0300

    project initializion

---
+++ modifiying-queries/src/main/java/com/incetutku/modifiyingqueries/ModifiyingQueriesApplication.java
@@ -0,0 +1,13 @@
+package com.incetutku.modifiyingqueries;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModifiyingQueriesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ModifiyingQueriesApplication.class, args);
+    }
+
+}
