755250bbea9b7221659321a7d95ebb7588e4e5ae
Tutku Ince <tutku.ince@outlook.com>
2025-03-27 23:52:53 +0300

    UserService persistUsers

--- modifiying-queries/src/main/java/com/incetutku/modifiyingqueries/service/UserService.java
+++ modifiying-queries/src/main/java/com/incetutku/modifiyingqueries/service/UserService.java
@@ -1,8 +1,13 @@
 package com.incetutku.modifiyingqueries.service;
 
+import com.incetutku.modifiyingqueries.entity.User;
 import com.incetutku.modifiyingqueries.repository.UserRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
 
 @Service
 public class UserService {
@@ -11,6 +16,17 @@ public class UserService {
 
     public UserService(UserRepository userRepository) {
         this.userRepository = userRepository;
+    }
+
+    @Transactional
+    public void persistUsers() {
+        User u1 = new User("John", LocalDate.of(2021, Month.AUGUST, 4), "john@email.com", 1, true);
+        User u2 = new User("Jane", LocalDate.of(2019, Month.MARCH, 18), "jane@email.com", 2, true);
+        User u3 = new User("Nicole", LocalDate.of(2017, Month.JULY, 21), "nicole@email.com", 1, false);
+        User u4 = new User("Ravi", LocalDate.of(2018, Month.JUNE, 15), "ravi@email.com", 1, false);
+        User u5 = new User("Alissa", LocalDate.of(2014, Month.APRIL, 5), "alissa@email.com", 2, true);
+
+        userRepository.saveAll(List.of(u1, u2, u3, u4, u5));
     }
 
     @Transactional
