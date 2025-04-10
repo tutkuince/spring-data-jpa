2c2a4ae2af5b1d8989c229eea3dbbd758f8ac1a1
Tutku Ince <tutku.ince@outlook.com>
2025-04-10 12:36:27 +0300

    prject initializatiıon

---
+++ projections/src/main/java/com/incetutku/projections/ProjectionsApplication.java
@@ -0,0 +1,13 @@
+package com.incetutku.projections;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectionsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectionsApplication.class, args);
+    }
+
+}
---
+++ projections/src/test/java/com/incetutku/projections/ProjectionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.incetutku.projections;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectionsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
