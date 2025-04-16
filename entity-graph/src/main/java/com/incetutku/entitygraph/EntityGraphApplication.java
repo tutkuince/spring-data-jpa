bd0a38f2d9cb4916f64a8306158372ce3bc10b87
Tutku Ince <tutku.ince@outlook.com>
2025-04-16 17:16:12 +0300

    project initializer

---
+++ entity-graph/src/main/java/com/incetutku/entitygraph/EntityGraphApplication.java
@@ -0,0 +1,13 @@
+package com.incetutku.entitygraph;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EntityGraphApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EntityGraphApplication.class, args);
+    }
+
+}
---
+++ entity-graph/src/test/java/com/incetutku/entitygraph/EntityGraphApplicationTests.java
@@ -0,0 +1,13 @@
+package com.incetutku.entitygraph;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EntityGraphApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
