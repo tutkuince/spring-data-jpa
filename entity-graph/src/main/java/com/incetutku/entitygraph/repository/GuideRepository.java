d73d25f6bc46556301730029a2b7f94ac5fb5037
Tutku Ince <tutku.ince@outlook.com>
2025-04-29 21:57:44 +0300

    new method for EntityGraph usage

--- entity-graph/src/main/java/com/incetutku/entitygraph/repository/GuideRepository.java
+++ entity-graph/src/main/java/com/incetutku/entitygraph/repository/GuideRepository.java
@@ -1,7 +1,12 @@
 package com.incetutku.entitygraph.repository;
 
 import com.incetutku.entitygraph.entity.Guide;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface GuideRepository extends JpaRepository<Guide, Long> {
+    @EntityGraph(value = "Guide.students", type = EntityGraph.EntityGraphType.LOAD)
+    Optional<Guide> findById(Long id);
 }
