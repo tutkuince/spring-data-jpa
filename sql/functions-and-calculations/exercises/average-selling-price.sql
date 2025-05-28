d2fc191d82e254676a47bff2e88c35c4e5db4f55
Tutku Ince <tutku.ince@outlook.com>
2025-05-28 13:40:02 +0300

    Average Selling Price

--- sql/functions-and-calculations/exercises/average-selling-price.sql
+++ sql/functions-and-calculations/exercises/average-selling-price.sql
@@ -29,3 +29,16 @@
 -- Write a solution to find the average selling price for each product. average_price should be rounded to 2 decimal places. If a product does not have any sold units, its average selling price is assumed to be 0.
 --
 -- Return the result table in any order.
+SELECT
+    p.product_id,
+    CASE
+        WHEN SUM(u.units) is NULL
+            THEN 0
+        ELSE
+            ROUND(SUM(u.units * p.price) / SUM(u.units)::numeric, 2)
+        END
+        AS average_price
+FROM Prices AS p
+         LEFT JOIN UnitsSold AS u
+                   ON p.product_id = u.product_id AND u.purchase_date BETWEEN p.start_date AND p.end_date
+GROUP BY p.product_id
