e226ab031364af194b4dfc589daa1f37589a3e76
Tutku Ince <tutku.ince@outlook.com>
2025-05-28 14:08:57 +0300

    Project Employees I

---
+++ sql/functions-and-calculations/exercises/project-employees-v1.sql
@@ -0,0 +1,38 @@
+-- Table: Project
+--
+-- +-------------+---------+
+-- | Column Name | Type    |
+-- +-------------+---------+
+-- | project_id  | int     |
+-- | employee_id | int     |
+-- +-------------+---------+
+-- (project_id, employee_id) is the primary key of this table.
+-- employee_id is a foreign key to Employee table.
+-- Each row of this table indicates that the employee with employee_id is working on the project with project_id.
+--
+--
+--
+-- Table: Employee
+--
+-- +------------------+---------+
+-- | Column Name      | Type    |
+-- +------------------+---------+
+-- | employee_id      | int     |
+-- | name             | varchar |
+-- | experience_years | int     |
+-- +------------------+---------+
+-- employee_id is the primary key of this table. It's guaranteed that experience_years is not NULL.
+-- Each row of this table contains information about one employee.
+--
+--
+--
+-- Write an SQL query that reports the average experience years of all the employees for each project, rounded to 2 digits.
+--
+-- Return the result table in any order.
+SELECT
+    p.project_id,
+    ROUND(SUM(e.experience_years ) / COUNT(p.project_id)::numeric, 2) as average_years
+FROM Project as p
+         LEFT JOIN Employee as e
+                   ON p.employee_id = e.employee_id
+GROUP BY p.project_id
