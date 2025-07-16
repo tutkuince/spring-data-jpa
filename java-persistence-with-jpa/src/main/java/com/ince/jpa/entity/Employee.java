9e1cd1002d08fe215e9f0b9ee99376fa2e572624
tutkuince <tutku.ince@outlook.com>
2025-07-16 22:05:06 +0300

    employee entity

---
+++ java-persistence-with-jpa/src/main/java/com/ince/jpa/entity/Employee.java
@@ -0,0 +1,79 @@
+package com.ince.jpa.entity;
+
+import jakarta.persistence.*;
+
+import java.io.Serializable;
+
+@Entity
+@Table(name = "employees")
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+public class Employee implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "employee_id")
+    private Long id;
+
+    @Column
+    private String name;
+
+    @Column
+    private String surname;
+
+    @Column
+    private Integer yearsExperience;
+
+    @Transient
+    private Double totalCompensation;
+
+    public Employee() {
+    }
+
+    public Employee(Long id, String name, String surname, Integer yearsExperience, Double totalCompensation) {
+        this.id = id;
+        this.name = name;
+        this.surname = surname;
+        this.yearsExperience = yearsExperience;
+        this.totalCompensation = totalCompensation;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public Integer getYearsExperience() {
+        return yearsExperience;
+    }
+
+    public void setYearsExperience(Integer yearsExperience) {
+        this.yearsExperience = yearsExperience;
+    }
+
+    public Double getTotalCompensation() {
+        return totalCompensation;
+    }
+
+    public void setTotalCompensation(Double totalCompensation) {
+        this.totalCompensation = totalCompensation;
+    }
+}
